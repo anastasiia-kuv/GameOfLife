d02fb74433f8a52eae854524c2d9b8fa722b475f
Unknown <leilani4551@gmail.com>
2018-09-18 17:00:51 +0700

    structure of BEM was updated

--- src/js/View.js
+++ src/js/View.js
@@ -5,30 +5,30 @@ export default function () {
   (function _initView () {
     const $body = $('body');
 
-    const pageContainer = document.createElement('div');
+    const pageContainer = document.createElement('main');
     pageContainer.className = 'page-container';
     document.body.insertBefore(pageContainer, document.body.firstChild);
 
     const nameOfGame = document.createElement('h1');
-    nameOfGame.className = 'title';
+    nameOfGame.className = 'page-container__title';
     nameOfGame.innerHTML = 'game of life';
     pageContainer.appendChild(nameOfGame);
 
-    const controlElementsContainer = document.createElement('div');
+    const controlElementsContainer = document.createElement('section');
     controlElementsContainer.className = 'control-elements-container';
     pageContainer.appendChild(controlElementsContainer);
 
     const widthForm = document.createElement('form');
-    widthForm.className = 'form width-form';
+    widthForm.className = 'width-form';
     controlElementsContainer.appendChild(widthForm);
 
     const widthLabel = document.createElement('label');
-    widthLabel.className = 'label width-form__label';
+    widthLabel.className = 'width-form__label';
     widthLabel.innerHTML = 'width';
     widthForm.appendChild(widthLabel);
 
     const widthInput = document.createElement('input');
-    widthInput.className = 'input width-form__input';
+    widthInput.className = 'width-form__input';
     widthInput.setAttribute('type', 'number');
     widthInput.setAttribute('value', '40');
     widthInput.setAttribute('tabindex', '1');
@@ -42,16 +42,16 @@ export default function () {
     widthForm.appendChild(widthInput);
 
     const heightForm = document.createElement('form');
-    heightForm.className = 'form height-form';
+    heightForm.className = 'height-form';
     controlElementsContainer.appendChild(heightForm);
 
     const heightLabel = document.createElement('label');
-    heightLabel.className = 'label height-form__label';
+    heightLabel.className = 'height-form__label';
     heightLabel.innerHTML = 'height';
     heightForm.appendChild(heightLabel);
 
     const heightInput = document.createElement('input');
-    heightInput.className = 'input height-form__input';
+    heightInput.className = 'height-form__input';
     heightInput.setAttribute('type', 'number');
     heightInput.setAttribute('value', '30');
     heightInput.setAttribute('tabindex', '2');
@@ -65,16 +65,16 @@ export default function () {
     heightForm.appendChild(heightInput);
 
     const speedForm = document.createElement('form');
-    speedForm.className = 'form speed-form';
+    speedForm.className = 'speed-form';
     controlElementsContainer.appendChild(speedForm);
 
     const speedLabel = document.createElement('label');
-    speedLabel.className = 'label speed-form__label';
+    speedLabel.className = 'speed-form__label';
     speedLabel.innerHTML = 'speed';
     speedForm.appendChild(speedLabel);
 
     const speedInput = document.createElement('input');
-    speedInput.className = 'input speed-form__input';
+    speedInput.className = 'speed-form__input';
     speedInput.setAttribute('type', 'range');
     speedInput.setAttribute('min', '1');
     speedInput.setAttribute('max', '10');
@@ -89,11 +89,11 @@ export default function () {
     speedForm.appendChild(speedInput);
 
     const newGameForm = document.createElement('form');
-    newGameForm.className = 'form new-game-form';
+    newGameForm.className = 'new-game-form';
     controlElementsContainer.appendChild(newGameForm);
 
     const newGameButton = document.createElement('input');
-    newGameButton.className = 'button new-game-form__button';
+    newGameButton.className = 'new-game-form__button';
     newGameButton.setAttribute('type', 'button');
     newGameButton.setAttribute('value', 'New game');
     newGameButton.setAttribute('tabindex', '4');
@@ -104,11 +104,11 @@ export default function () {
     newGameForm.appendChild(newGameButton);
 
     const startGameForm = document.createElement('form');
-    startGameForm.className = 'form start-game-form';
+    startGameForm.className = 'start-game-form';
     controlElementsContainer.appendChild(startGameForm);
 
     const startGameButton = document.createElement('input');
-    startGameButton.className = 'button start-game-form__button';
+    startGameButton.className = 'start-game-form__button';
     startGameButton.setAttribute('type', 'button');
     startGameButton.setAttribute('value', 'Start');
     startGameButton.setAttribute('tabindex', '5');
@@ -119,11 +119,11 @@ export default function () {
     startGameForm.appendChild(startGameButton);
 
     const pauseGameForm = document.createElement('form');
-    pauseGameForm.className = 'form pause-game-form';
+    pauseGameForm.className = 'pause-game-form';
     controlElementsContainer.appendChild(pauseGameForm);
 
     const pauseGameButton = document.createElement('input');
-    pauseGameButton.className = 'button pause-game-form__button';
+    pauseGameButton.className = 'pause-game-form__button';
     pauseGameButton.setAttribute('type', 'button');
     pauseGameButton.setAttribute('value', 'Pause');
     pauseGameButton.setAttribute('tabindex', '6');
@@ -134,7 +134,7 @@ export default function () {
     pauseGameForm.appendChild(pauseGameButton);
 
     canvas = document.createElement('canvas');
-    canvas.className = 'canvas';
+    canvas.className = 'page-container__canvas';
     canvas.setAttribute('width', widthInput.value * 10);
     canvas.setAttribute('height', heightInput.value * 10);
     canvas.onclick = function onClickCanvas(e) {
